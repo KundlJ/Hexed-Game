d3d17deb043c801b4f507924c5430e4a42b3afeb
Jed K <KundlJ@GMail.com>
2013-10-20 17:07:34 -0400

    Slider-input.js

---
+++ slider-input.js
@@ -0,0 +1,81 @@
+// Vairables for input fields
+var red = document.getElementById("red");
+var green = document.getElementById("green");
+var blue = document.getElementById("blue");
+
+// Variables for warning messages
+var foo = document.getElementById("rbadval");
+foo.style.visibility = 'hidden';
+
+var foo = document.getElementById("gbadval");
+foo.style.visibility = 'hidden';
+
+var foo = document.getElementById("bbadval");
+foo.style.visibility = 'hidden';
+
+// Functions for if value is changed in the input field
+red.onchange = function(e) {
+	var num = $("#red").val(); 
+
+	if ((isNaN(num)) || (num < 0 || num > 255)){
+		var foo = document.getElementById("rbadval");
+		foo.style.visibility = 'visible';
+	}
+
+	else {
+		num = num * 100/255;
+		$("#redslider").slider("value", num);
+		var foo = document.getElementById("rbadval");
+		foo.style.visibility = 'hidden';
+	}
+};
+
+green.onchange = function(e) {
+	var num = $("#green").val(); 
+
+	if ((isNaN(num)) || (num < 0 || num > 255)){
+		var foo = document.getElementById("gbadval");
+		foo.style.visibility = 'visible';
+	}
+
+	else {
+		num = num * 100/255;
+		$("#greenslider").slider("value", num);
+		var foo = document.getElementById("gbadval");
+		foo.style.visibility = 'hidden';
+	}
+};
+
+blue.onchange = function(e) {
+	var num = $("#blue").val(); 
+
+	if ((isNaN(num)) || (num < 0 || num > 255)){
+		var foo = document.getElementById("bbadval");
+		foo.style.visibility = 'visible';
+	}
+
+	else {
+		num = num * 100/255;
+		$("#blueslider").slider("value", num);
+		var foo = document.getElementById("bbadval");
+		foo.style.visibility = 'hidden';
+	}
+};
+
+// Functions for if the value is changed using the slider
+
+$( "#redslider" ).on( "slidechange", function( event, ui ) {
+	num = Math.floor(ui.value * 255 / 100);
+	red.value = num;
+} );
+
+$( "#greenslider" ).on( "slidechange", function( event, ui ) {
+	num = Math.floor(ui.value * 255 / 100);
+	green.value = num;
+} );
+
+$( "#blueslider" ).on( "slidechange", function( event, ui ) {
+	num = Math.floor(ui.value * 255 / 100);
+	blue.value = num;
+} );
+
